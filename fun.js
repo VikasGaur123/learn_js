f2fd62f3f4b5fbc971c2860ffa51b8490bb0f89d
Vikas gaur <vikas.gaur@deorwine.com>
2022-09-08 19:27:24 +0530

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,6 +1,27 @@
-function showMessage() {
-    console.log( 'Hello sir!' );
+let str="3030+40000";
+let l=str.length;
+let=num1="";
+let=num2="";
+for(i=0;i<=l;i++)
+{
+  if('+'==str[i])
+  {
+
+    for(j=i+1;j<l;j++)
+    {
+      num2+=str[j];
+    }
+    break;
   }
-  showMessage();
-let Newmess = ()=> console.log('Hello sir!');
-Newmess();
+  else{
+    num1+=str[i];
+  }
+}
+console.log(num1);
+console.log(num2);
+let num11=Number(num1);
+let num22=Number(num2);
+console.log(typeof num11);
+console.log(typeof num22);
+let total=num11+num22;
+console.log(total);
